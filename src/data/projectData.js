df68f98e603581ea24ce3c7d87fad21296f747ec
Dilshan Gamage <randikasandaruwan917@gmail.com>
2025-03-02 08:42:50 +0530

    ongoing projects add

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,31 +1,215 @@
-import img88 from "../assest/package_images/88.jpg";
-import img92 from "../assest/package_images/92.jpg";
-import kelaniya from "../assest/project_images/kelaniya.jpg";
-import wattala from "../assest/project_images/wattala.jpg";
-import miriswatta from "../assest/project_images/miriswatta.jpg";
+import weligampitiya1 from "../assest/project_images/Weligampitiya/1.jpg";
+import weligampitiya2 from "../assest/project_images/Weligampitiya/2.jpg";
+import weligampitiya3 from "../assest/project_images/Weligampitiya/3.jpg";
+
+import miriswatta1 from "../assest/project_images/Miriswatta/1.jpg";
+import miriswatta2 from "../assest/project_images/Miriswatta/2.jpg";
+import miriswatta3 from "../assest/project_images/Miriswatta/3.jpg";
+
+import rathmalana1 from "../assest/project_images/Rathmalana/1.jpg";
+import rathmalana2 from "../assest/project_images/Rathmalana/2.jpg";
+import rathmalana3 from "../assest/project_images/Rathmalana/3.jpg";
+
+import panadura_2_1 from "../assest/project_images/Panadura(2)/1.jpg";
+import panadura_2_2 from "../assest/project_images/Panadura(2)/2.jpg";
+import panadura_2_3 from "../assest/project_images/Panadura(2)/3.jpg";
+
+import rilaulla_2_1 from "../assest/project_images/Rilaulla(2)/1.jpg";
+import rilaulla_2_2 from "../assest/project_images/Rilaulla(2)/2.jpg";
+import rilaulla_2_3 from "../assest/project_images/Rilaulla(2)/3.jpg";
+
+import wattala1 from "../assest/project_images/Wattala/1.jpg";
+import wattala2 from "../assest/project_images/Wattala/2.jpg";
+import wattala3 from "../assest/project_images/Wattala/3.jpg";
+
+import kelaniya1 from "../assest/project_images/Kelaniya/1.jpg";
+import kelaniya2 from "../assest/project_images/Kelaniya/2.jpg";
+import kelaniya3 from "../assest/project_images/Kelaniya/3.jpg";
+
+import kadawatha1 from "../assest/project_images/Kadawatha/1.jpg";
+import kadawatha2 from "../assest/project_images/Kadawatha/2.jpg";
+import kadawatha3 from "../assest/project_images/Kadawatha/3.jpg";
+
+import mahara1 from "../assest/project_images/Mahara/1.jpg";
+import mahara2 from "../assest/project_images/Mahara/2.jpg";
+import mahara3 from "../assest/project_images/Mahara/3.jpg";
+
+import dompe1 from "../assest/project_images/Dompe/1.jpg";
+import dompe2 from "../assest/project_images/Dompe/2.jpg";
+import dompe3 from "../assest/project_images/Dompe/3.jpg";
+
+import homagama1 from "../assest/project_images/Homagama/1.jpg";
+import homagama2 from "../assest/project_images/Homagama/2.jpg";
+import homagama3 from "../assest/project_images/Homagama/3.jpg";
+
+import weliweriya_1_1 from "../assest/project_images/Weliweriya(1)/1.jpg";
+import weliweriya_1_2 from "../assest/project_images/Weliweriya(1)/2.jpg";    
+import weliweriya_1_3 from "../assest/project_images/Weliweriya(1)/3.jpg";
+
+import panadura_1_1 from "../assest/project_images/Panadura(1)/1.jpg";
+import panadura_1_2 from "../assest/project_images/Panadura(1)/2.jpg";
+import panadura_1_3 from "../assest/project_images/Panadura(1)/3.jpg";
 
 
 const projectdata = [
+    
     {
         id: 1,
-        site: "Project Site 1",
+        site: "Kadawatha",
+        location: "https://maps.app.goo.gl/pdPFG69W45iWB4zZ6",
+        images: [
+            kadawatha1,
+            kadawatha2,
+            kadawatha3
+            
+        ]
+    },
+
+    {
+        id: 2,
+        site: "Miriswatta",
+        location: "https://maps.app.goo.gl/igzCZmLWXSNGQtWE8",
+        images: [
+            miriswatta1,
+            miriswatta2,
+            miriswatta3
+            
+        ]
+    },
+
+    {
+        id: 3,
+        site: "Rathmalana",
+        location: "https://maps.app.goo.gl/meDL6ChTPdzEmUVv7",
+        images: [
+            rathmalana1,
+            rathmalana2,
+            rathmalana3
+            
+        ]
+    },
+
+    {
+        id: 4,
+        site: "Panadura 2",
+        location: "https://maps.app.goo.gl/vFLZ3MJhVjPPdoAC8",
+        images: [
+            panadura_2_1,
+            panadura_2_2,
+            panadura_2_3
+            
+        ]
+    },
+
+    {
+        id: 5,
+        site: "Rilaulla 2",
+        location: "https://maps.app.goo.gl/irAuCVU7gGuS72cq5",
+        images: [
+            rilaulla_2_1,
+            rilaulla_2_2,
+            rilaulla_2_3
+            
+        ]
+    },
+
+    {
+        id: 6,
+        site: "Wattala",
+        location: "https://maps.app.goo.gl/8C9Fna6RikiiZ9Bb8?g_st=com.google.maps.preview.copy",
+        images: [
+            wattala1,
+            wattala2,
+            wattala3
+            
+        ]
+    },
+
+    {
+        id: 7,
+        site: "Kelaniya",
+        location: "https://maps.app.goo.gl/S2WsMXjL5hZTvzUm7",
+        images: [
+            kelaniya1,
+            kelaniya2,
+            kelaniya3
+            
+        ]
+    },
+
+    {
+        id: 8,
+        site: "Weligampitiya",
         location: "https://maps.app.goo.gl/wJS87YYDS1jVRnnn9?g_st=com.google.maps.preview.copy",
         images: [
-          img88,
-          img92,
-          img88
+            weligampitiya1,
+            weligampitiya2,
+            weligampitiya3
         ]
-      },
+    },
+
+
+    {
+        id: 9,
+        site: "Mahara",
+        location: "https://maps.app.goo.gl/6SnNTjVnc8zRH4qm7",
+        images: [
+            mahara1,
+            mahara2,
+            mahara3
+            
+        ]
+    },
+
+    {
+        id: 10,
+        site: "Dompe",
+        location: "https://maps.app.goo.gl/fmRbskQMae6FYrYa6",
+        images: [
+            dompe1,
+            dompe2,
+            dompe3
+            
+        ]
+    },
+
+    {
+        id: 11,
+        site: "Homagama",
+        location: "https://maps.app.goo.gl/7L6w5FCrUNYrP8d5A",
+        images: [
+            homagama1,
+            homagama2,
+            homagama3
+            
+        ]
+    },
+
+    {
+        id: 12,
+        site: "Weliweriya 1",
+        location: "https://maps.app.goo.gl/DVadhnw9DKmVdhTj6",
+        images: [
+            weliweriya_1_1,
+            weliweriya_1_2,
+            weliweriya_1_3
+            
+        ]
+    },
+
+    {
+        id: 13,
+        site: "Panadura 1",
+        location: "https://goo.gl/maps/ZuviK4inZeeGCoCa6",
+        images: [
+            panadura_1_1,
+            panadura_1_2,
+            panadura_1_3
+            
+        ]
+    },
       
-    // { id: 1, location: "Miriswatta, Sri Lanka.", image: miriswatta },
-    // { id: 2, location: "Kelaniya, Sri Lanka.", image: kelaniya },
-    // { id: 3, location: "Wattala, Sri Lanka.", image: wattala },
     
-    // { id: 4, location: "Negombo, Sri Lanka", image: img88 },
-    // { id: 5, location: "Moratuwa, Sri Lanka", image: img92 },
-    // { id: 6, location: "Negombo, Sri Lanka", image: img88 },
-    // { id: 7, location: "Negombo, Sri Lanka", image: img88 },
-    // { id: 8, location: "Negombo, Sri Lanka", image: img88 },
     ];
 
 export default projectdata;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,7 +9,7 @@ import { Heading } from "../components/Heading";
 export const Contact = () => {
   return (
     <div className="contact">
-      <Heading title="Contact us" />
+      <Heading title="Contact us" data-aos="fade-down"/>
       <div className="container py-4 blur-box">
         <div className="row align-items-center">
           {/* Contact Info Section */}
@@ -25,21 +25,24 @@ export const Contact = () => {
                 }}
               />
             </h2>
-            <p>
+            <p data-aos="fade-up-right">
               <FaHouseChimney className="text-dark me-2" /> <strong>Address:<br/></strong>
               NO:72, Dandugama, Ja-Ela, Sri Lanka.
             </p>
-            <p>
+            <hr></hr>
+            <p data-aos="fade-up-left">
               <FaPhoneSquareAlt className="text-success me-2" /> <strong>Call:<br/></strong>
               <strong><a href="tel:+94 113771045" className="text-light">0113771045</a></strong> | {" "}
               <strong><a href="tel:+94 768763292" className="text-light">0768763292</a></strong> | {" "}
               <strong><a href="tel:+94 713750557" className="text-light">0713750557</a></strong>
             </p>
-            <p className="email">
+            <hr />
+            <p className="email" data-aos="fade-up-right">
               <FaEnvelope className="text-danger me-2 email-icon" /> <strong>Email:<br/></strong>
               <a href="mailto:unitybuilderslanka@gmail.com" className="text-light">unitybuilderslanka@gmail.com</a>
             </p>
-            <p className="facebook">
+            <hr />
+            <p className="facebook" data-aos="fade-up-left">
               <FaFacebook className="me-2 text-primary fb-icon" /> <strong>Facebook:<br/></strong>
               <a
                 href="https://facebook.com/UnityBuildersLanka"
@@ -48,7 +51,8 @@ export const Contact = () => {
                 className="text-light fw-bold"
               >Unity Builders Lanka</a>
             </p>
-            <p className="website">
+            <hr />
+            <p className="website" data-aos="fade-up-right">
               <AiOutlineGlobal className="text-dark web-icon" /> <strong>Website:<br/></strong>
               <a href="https://www.unitybuilderslanka.com" className="text-light">www.unitybuilderslanka.com</a>
             </p>
@@ -57,7 +61,7 @@ export const Contact = () => {
           {/* Google Map Section */}
           <div className="col-lg-6 map-info">
             <h2 className="mb-4 fw-bold text-center">Location</h2>
-            <div className="map-container">
+            <div className="map-container" data-aos="flip-left">
               <iframe 
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.252173250844!2d79.8865237!3d7.0967429!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae2f10063b2ea3b%3A0x6cd5f1699e850aff!2sUnity%20Builder's%20Lanka!5e0!3m2!1sen!2slk!4v1740861450127!5m2!1sen!2slk" 
                 width="100%" 
