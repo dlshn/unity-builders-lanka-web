1b6d7cf568693b63b918c185f7b95e389fa85f6f
Dilshan Randika Sandaruwan <randikasandaruwan917@gmail.com>
2025-03-13 07:31:54 +0530

    Project design changed

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Heading } from "../components/Heading";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap
+import { HiLocationMarker } from "react-icons/hi";
 import "../assest/styles/projects.css"; // Custom styles
 import projectData from "../data/projectData"; // Project data
 
@@ -10,9 +11,10 @@ export const Project = () => {
       <Heading title="Latest Projects" data-aos="fade-down" />
       <div className="container">
         {/* Bootstrap Grid */}
-        <div className="row g-4">
+        <div className="row g-4 d-flex align-items-center justify-content-between">
           {projectData.map((item) => (
-            <div key={item.id} className="col-lg-4 col-md-6 col-sm-12">
+            <div key={item.id} className="col-lg-4 col-md-6 col-sm-12 ">
+              <div className="project-outer d-flex flex-column align-items-center">
               <div className="project-box" data-aos={item.id % 2 === 0 ? "fade-right" : "fade-left"}>
                 {/* Bootstrap Carousel for Multiple Images */}
                 <div id={`carouselExample${item.id}`} className="carousel slide" data-bs-ride="carousel">
@@ -33,11 +35,16 @@ export const Project = () => {
                     <span className="visually-hidden">Next</span>
                   </button>
                 </div>
-                <div className="overlay">
+                {/* <div className="overlay">
                   <h4>{item.site}</h4>
-                  <a href={item.location} type="button" class="btn btn-primary">Location</a>
-                </div>
+                  <a href={item.location} type="button" className="btn btn-primary">Location</a>
+                </div> */}
               </div>
+              <div className="d-flex flex-row" data-aos="fade-left"><HiLocationMarker  className="text-light m-auto fs-3" /><h4 className="location-text text-light mt-2 mx-2" >{item.site}</h4></div>
+              <a href={item.location} type="button" className="location-btn btn mb-2" data-aos="fade-right">Location</a>
+              </div>
+              
+              
             </div>
           ))}
         </div>
